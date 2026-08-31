277c17adf70e76d856cb9ccdbbe3e2f9e23c972e
Joni Jessica Mamilla <luckyjoni186@gmail.com>
2026-09-01 00:09:15 +0530

    Time: 1 ms (99.94%), Space: 77.3 MB (32.52%) - LeetHub-Neo

---
+++ LeetCode/0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int n=nums.length;
+        int sum=nums[0],maxSum=nums[0];
+        for(int i=1;i<n;i++){
+            sum=Math.max(nums[i],sum+nums[i]);
+            maxSum=Math.max(sum,maxSum);
+        }
+        return maxSum;
+    }
+}
