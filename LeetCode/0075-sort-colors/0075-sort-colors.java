6eae7eb7f7325e5c7d3c69ff5cdb77f1521f03fa
Joni Jessica Mamilla <luckyjoni186@gmail.com>
2026-09-01 00:08:50 +0530

    Time: 0 ms (100%), Space: 43 MB (97.53%) - LeetHub-Neo

---
+++ LeetCode/0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        //Arrays.sort(nums);
+        int n=nums.length;
+        int[] cnt=new int[3];
+        for(int i:nums) cnt[i]++;
+        int idx=0;
+        for(int i=0;i<3;i++){
+            int val=cnt[i];
+            while(val>0){
+                nums[idx++]=i;
+                val--;
+            }
+        }
+    }
+}
